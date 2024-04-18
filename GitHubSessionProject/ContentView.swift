378d063ebf467c784578e12f0ed899a9950c6d6a
jongchang <hokhmah91@naver.com>
2024-04-18 20:40:29 +0900

    Second commit

--- GitHubSessionProject/ContentView.swift
+++ GitHubSessionProject/ContentView.swift
@@ -14,6 +14,11 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Jomir!")
+            Text("Hello, Jomir!")
+            Text("Hello, Jomir!")
+            Text("Hello, Jomir!")
+            Text("Hello, Jomir!")
+            
         }
         .padding()
     }
